5b4148d904e11bd329304027559f3f3b18ff3e20
Max Douglas <maxmdps@gmail.com>
2019-07-28 22:00:11 -0700

    Create tests for RssList

--- rss_reader.py
+++
@@ -1,6 +0,0 @@
-class RssReader:
-    file = "rss.txt"
-    @staticmethod
-    def read():
-        with open(RssReader.file) as urls:
-            return [rss.strip() for rss in urls.read().split('\n') if rss]
--- test/test_rss_list.py
+++ test/test_rss_list.py
@@ -2,8 +2,9 @@ from rss_list import RssList
 
 
 class TestRssList:
-    RssList._file = "test_%s" % RssList._file
-    rss_list = RssList.get()
-    assert type(rss_list) is list
-    assert len(rss_list) is 4
-    assert rss_list[2] == "some_site"
+    def test_rss_list(self):
+        RssList._file = "test_%s" % RssList._file
+        rss_list = RssList.get()
+        assert type(rss_list) is list
+        assert len(rss_list) is 4
+        assert rss_list[2] == "some_site"
